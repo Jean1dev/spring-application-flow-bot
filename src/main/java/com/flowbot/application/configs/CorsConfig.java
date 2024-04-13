53f4d2553c4be357e43990832e05d4ef20b0ea8b
augusto.savi <augusto.savi@betha.com.br>
2024-04-13 14:24:50 -0300

    #: Clean: limpando import de dependencias não mais necessarias

--- src/main/java/com/flowbot/application/configs/CorsConfig.java
+++ src/main/java/com/flowbot/application/configs/CorsConfig.java
@@ -2,10 +2,8 @@ package com.flowbot.application.configs;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
